64682850925b41e303c22fee4b05c0dc71b7fae5
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-01-12 14:09:46 +0530

    Record

---
+++ Arrays/InsertionSort.java
@@ -0,0 +1,19 @@
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
