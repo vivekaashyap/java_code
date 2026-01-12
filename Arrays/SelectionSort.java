8d888ef982e4b1954e6750afad079fcc2cc9efdc
Vivek Kashyap <vivekkashyap8299@gmail.com>
2026-01-12 13:44:53 +0530

    Add Question

---
+++ Arrays/SelectionSort.java
@@ -0,0 +1,21 @@
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minpos] > arr[j]) {
+                    minpos = j;
+                }
+            }
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
